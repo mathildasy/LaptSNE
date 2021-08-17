e749a072f23c4dbb065ba2ced25370504254ee92
Yan Sun <119020045@link.cuhk.edu.cn>
2021-08-17 09:39:05 +0800

    update 8-17

--- code/test_2.py
+++ code/test_2.py
@@ -23,33 +23,43 @@ MOMENTUM = 0.8
 NUM_ITERS = 100
 TSNE = True
 TSNE_grad = True
+EXA_RATIO = 4
 NUM_PLOTS = 3
 
-data = datasets.load_iris()
-NUM_SAMPLE = 400
-PERPLEXITY = 40
-LEARNING_RATE = 1e1 # change with NUM_SAMPLE
-EXAGGERATE_STAGE = 50 # change with NUM_SAMPLE
-LAST_STAGE = 5 # change with NUM_SAMPLE
-NUM_EIGEN = 4
-RDSEED = 1
-GK = 1
-BETA = 1e-2  # change with delta Y
+# data = datasets.load_iris()
+# NUM_SAMPLE = 400
+# PERPLEXITY = 40
+# LEARNING_RATE = 1e1 # change with NUM_SAMPLE
+# EXAGGERATE_STAGE = 50 # change with NUM_SAMPLE
+# LAST_STAGE = 5 # change with NUM_SAMPLE
+# NUM_EIGEN = 4
+# RDSEED = 1
+# GK = 1
+# BETA = 1e-2  # change with delta Y
 
 # data = datasets.load_digits()
-# PERPLEXITY = 20
+# NUM_SAMPLE = 400
+# PERPLEXITY = 25
 # LEARNING_RATE = 1e2  # change with NUM_SAMPLE
 # EXAGGERATE_STAGE = 50  # change with NUM_SAMPLE
 # LAST_STAGE = 5  # change with NUM_SAMPLE
 # GK = 1
-# BETA = 1e-2  # change with delta Y
-# NUM_EIGEN = 12
+# BETA = 1e0  # change with delta Y
+# NUM_EIGEN = 11
 # RDSEED = 1
-# GK = 0
-# BETA = 1e-5  # change with delta Y
 
+data = datasets.load_digits()
+NUM_SAMPLE = 800
+PERPLEXITY = 30
+LEARNING_RATE = 1e2  # change with NUM_SAMPLE
+EXAGGERATE_STAGE = 50  # change with NUM_SAMPLE（不需要太大）
+LAST_STAGE = 20  # change with NUM_SAMPLE （到底会不会过头呢？）
+GK = 1
+BETA = 3e0  # change with delta Y （较为敏感）
+NUM_EIGEN = 12
+RDSEED = 1
 
-NUM_SAMPLE = 400
+print(data['data'].shape)
 X = data['data'][:NUM_SAMPLE]
 y = data['target'][:NUM_SAMPLE]
 
@@ -70,7 +80,8 @@ Y, lam_list = ts.estimate_sne(X, y, P,
                      exa_stage=EXAGGERATE_STAGE,
                      lst_stage=LAST_STAGE,
                      rdseed=RDSEED,
-                     sigmas=sigmas)
+                     sigmas=sigmas,
+                     exa_ratio = EXA_RATIO)
 
 
 lam_df = pd.DataFrame(lam_list).T
